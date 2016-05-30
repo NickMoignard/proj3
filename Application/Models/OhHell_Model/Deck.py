ce1d0de87e34fc5b4c088aeee74f41ce1c1d5062
NickMoignard <nicklmoignard@gmail.com>
2016-05-31 01:48:31 +1000

    completed the deck of cards class, which can be used for many different card games not just oh hell

--- Application/Models/OhHell_Model/Deck.py
+++ Application/Models/OhHell_Model/Deck.py
@@ -1,13 +1,16 @@
-from itertool import cycle
-import OhHell_Tools
-from copy import deepcopy
 
-class Deck(self):
+from OhHell_Tools import OhHell_Tools
+from copy import deepcopy
+from random import shuffle
+
+class Deck():
 	def __init__(self):
+		self.tools = OhHell_Tools()  # think this might be bad practice
 		self._cards_in_deck = list(range(52))
 		self._cards_in_hands = []
 		self._discarded_cards = []
 		self._no_card_packs_used = 1
+		self.shuffle_deck()
 
 	def add_card_pack_to_deck(self):
 		""" name: add_card_pack_to_deck()
@@ -15,44 +18,29 @@ class Deck(self):
 			input(s): None
 			output(s): None
 		"""
+		
 		self._cards_in_deck += list(range(52))
 		self._no_card_packs_used += 1
 		self.shuffle_deck()
 
-	def deal(self, no_cards, seats=1):
-		""" name: deal()
-			synopsis: deal x number of cards from deck to n number of seats
-			input(s):
-				no_cards an integer,
-				Optional Arg seats, takes an int, will deal x no cards to n
-				number of seats
-			outputs(s):
-				a list of cards or nested lists of dealt cards for each player
+	def deal_card(self):
+		""" name: deal_card()
+			synopsis: deal single card from deck
+			input(s): None
+			outputs(s): string represntation of card
 		"""
-		deck = self._cards_in_deck
 
 		# Check if enough cards to deal
-		tot_cards_needed = no_cards*seats
-		if tot_cards_needed > len(deck):
+		if len(self._cards_in_deck) == 0 and len(self._discarded_cards) == 0:
+			self.add_card_pack_to_deck()
+		elif len(self._cards_in_deck) == 0:
 			self.shuffle_deck()  # Shuffle in burnt cards
-			if tot_cards_needed > len(deck):
-				self.add_card_pack_to_deck()
+			
 				
+		card = self._cards_in_deck.pop()
+		self._cards_in_hands.append(deepcopy(card))
 
-		# deal cards from deck
-		if seats == 1:
-			cards_to_deal = [deck.pop() for _ in range(no_cards)]
-		else:
-			cards_to_deal = [
-				[deck.pop() for _ in range(no_cards)] for seat in range(seats)
-			]
-
-		# Add dealt cards to cards_in_hands to keep track of each card in deck
-		cards = deepcopy(cards_to_deal)
-		cards = OhHell_Tools.sort_cards(cards)
-		self._cards_in_hands += cards
-
-		return cards_to_deal
+		return self.tools.int_to_str(card)
 
 	def burn(self, no_cards):
 		""" name: burn()
@@ -61,14 +49,14 @@ class Deck(self):
 			output(s): None
 		"""
 		# check if enough cards in deck to burn
-		if no_cards > len(deck):
+		while no_cards > len(self._cards_in_deck):
 			# Shuffle burnt cards back into deck
 			self.shuffle_deck()
-			if no_cards > len(deck):
+			if no_cards > len(self._cards_in_deck):
 				# Add additional pack of cards to deck
 				self.add_card_pack_to_deck()
 
-		cards_to_burn = [self.cards_in_deck.pop() for _ in range(no_cards)]
+		cards_to_burn = [self._cards_in_deck.pop() for _ in range(no_cards)]
 		self._discarded_cards += cards_to_burn
 
 	def shuffle_deck(self):
@@ -78,6 +66,7 @@ class Deck(self):
 			output(s): None
 		"""
 		deck = self._cards_in_deck + self._discarded_cards
+		self._discarded_cards = []
 		shuffle(deck)
 		self._cards_in_deck = deck
 
@@ -87,28 +76,97 @@ class Deck(self):
 			input(s): card_to_discard, a string representation of a card
 			output(s): None
 		"""
+		card = self.tools.str_to_int(card_to_discard)
+
 		if card in self._cards_in_hands:
 			self._cards_in_hands.remove(card)
-			self._discarded_cards += card
+			self._discarded_cards.append(card)
 		else:
 			raise ValueError(
 				'{} is not currently in any players hands'.format(card)
 			)
 
-
+# perfom tests
 if __name__ == "__main__":
-	# create a deck
-	new_deck = Deck()
 
-	deck_top = new_deck.deal(1)
-	# deal one card
-	# deal 10 cards
-	# deal 10 cards with 4 seats 
-	# burn
-	# shuffle
-	# discard
+	players_hands = [[], [], [], []]
+	passed = 0
+	deck = Deck()
 
+	# Test: Deal one card
+	card = deck.deal_card()
+	if len(deck._cards_in_deck) == 51 and len(deck._cards_in_hands) == 1:
+		passed += 1
 
+	del deck
+	deck = Deck()
+
+	# Test: Deal Mutiple cards
+	for _ in range(2):
+		for i in range(4):
+			players_hands[i].append(deck.deal_card())
+
+	if len(deck._cards_in_deck) == (44) and len(deck._cards_in_hands) == 8:
+		passed += 1
+
+	# Test: Discard card from a hand
+	rm_card = players_hands[0].pop()
+	deck.discard(rm_card)
+
+	if len(deck._discarded_cards) == 1 and len(deck._cards_in_hands) == 7:
+		# discard a card
+		passed += 1
+
+	del deck
+	players_hands = [[], [], [], []]
+	deck = Deck()
+
+	# Test: Burn cards from top of deck
+	deck.burn(10)
+	if len(deck._discarded_cards) == 10 and len(deck._cards_in_deck) == 42:
+		# discard a card
+		passed += 1
+
+	# Test: Shuffle the burnt cards back into deck
+	deck.shuffle_deck()
+	if len(deck._cards_in_deck) == 52 and len(deck._discarded_cards) == 0:
+		passed += 1
+
+	# Test: Multiple Decks
+	deck.add_card_pack_to_deck()
+	if len(deck._cards_in_deck) == 104 and deck._no_card_packs_used == 2:
+		passed += 1
+
+	del deck
+	deck = Deck()
+
+	# Test: Deal cards until additional deck is added
+	for _ in range(100):
+		players_hands[0].append(deck.deal_card())
+	if len(deck._cards_in_deck) == 4 and len(deck._cards_in_hands) == 100:
+		passed += 1
+
+	# Test: Discarding with multiple decks
+	for _ in range(100):
+		rm_card = players_hands[0].pop()
+		deck.discard(rm_card)
+	if len(deck._cards_in_hands) == 0 and len(deck._discarded_cards) == 100:
+		passed += 1
+
+	# Test: Shuffle Cards back into deck
+	deck.shuffle_deck()
+	if len(deck._cards_in_deck) == 104 and len(deck._discarded_cards) == 0:
+		passed += 1
+
+	del deck
+	deck = Deck()
+
+	# Test: Burn cards until additional deck is added
+	deck.burn(53)
+	if len(deck._cards_in_deck) == 51 and len(deck._discarded_cards) == 53:
+		passed += 1
+
+	print('Deck: {} out of 10 tests passed'.format(passed))
 
 
 
